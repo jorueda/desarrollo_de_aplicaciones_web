be358de56c42497b527e8b2ce2a06671ab7e839f
Jorge Rueda <jhrueda@uninorte.edu.co>
2021-11-17 07:40:45 -0500

    Adicion comentarios

--- componente_practico_01/index.js
+++ componente_practico_01/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-app.use(express.json())
+app.use(express.json()) // Requerido para evitar el error TypeError: Cannot destructure property 'a' of 'peticion.body' as it is undefined.
 
 app.get('/', (peticion, respuesta) => {
     respuesta.sendFile(path.join(__dirname, 'index.html'))
@@ -15,6 +15,6 @@ app.post('/suma', (peticion, respuesta) => {
     console.log(result)
 })
 
-app.listen(5000, () => {
+app.listen(5000, () => { // Solamente funciona en http://localhost:5000/ no en http://127.0.0.1:5000/
     console.log("Servidor escuchando por el puerto 5000")
 })
