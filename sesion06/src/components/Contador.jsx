352fb934a4590852b9b4f9f4ec357c7d2099f29c
Jorge Rueda <jhrueda@uninorte.edu.co>
2021-11-22 09:44:12 -0500

    Agregar Timeout al contador

--- sesion06/src/components/Contador.jsx
+++ sesion06/src/components/Contador.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export function Contador() {
     const [cuenta, setCuenta] = useState(0);
+
+    // Luego de 2 segundos regresa la variable en 20.
+    useEffect(() => {
+        const esperar = setTimeout(() => {
+            setCuenta(20);
+        }, 2000);
+
+        return () => clearTimeout(esperar);
+    });
 
     return (
         <div className='mb-2 p-2'>
