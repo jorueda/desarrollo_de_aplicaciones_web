7b838770f2da1eb4faacc8ca45a38855ddfb3052
Jorge Rueda <jhrueda@uninorte.edu.co>
2021-11-22 08:48:51 -0500

    Adicion componente funcional

--- sesion06/src/App.js
+++ sesion06/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Prueba from './components/Prueba';
+import Saludo from './components/Saludar';
 
 function App() {
+  const buenas = 'Este es otro saludo';
+  const props = {msg: 'Hola de nuevo!!'};
+
   return (
     <div className="App">
       <header className="App-header">
+        <Saludo msg='Hola a todos' />
+        <Saludo msg={buenas} />
+        <Saludo {...props} />
         <img src={logo} className="App-logo" alt="logo" />
         <Prueba />
         <p>
---
+++ sesion06/src/components/Saludar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Saludo = ({msg, ...rest}) => <h1>{msg}</h1>;
+
+export default Saludo;
